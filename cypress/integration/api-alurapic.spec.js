10931d209e4bbd65f39e9b05714d27bb0aed717d
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-03-14 22:09:46 -0300

    Testando dados sensíveis

--- cypress/integration/api-alurapic.spec.js
+++ cypress/integration/api-alurapic.spec.js
@@ -20,7 +20,25 @@ describe("Busca fotos e dados ",()=>{
     )
 
 
+    it.only('Fazer login do Flavio',()=>{
 
+        cy.request({
+    
+              method: 'POST',
+              url: 'https://apialurapic.herokuapp.com/user/login',
+              body: Cypress.env()
+
+        }).then((res)=>{
+    
+             expect(res.status).to.be.equal(200)
+             expect(res.body).is.not.empty
+             expect(res.body).to.have.property('id')
+             expect(res.body.id).to.be.equal(1)
+             expect(res.body).to.have.property('email')
+             expect(res.body.email).to.be.equal("flavio@alurapic.com.br")
+        }
+    
+        )
 
 
   })
